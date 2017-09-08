332d80af7b1046cf9b5e62808959bb5e2652bf16
Bopas <nazihbopas@gmail.com>
2017-09-08 14:14:38 +0700

    Pertalit
    update pertama

--- application/modules/base/views/templates/spacious/main.php
+++ application/modules/base/views/templates/spacious/main.php
@@ -1,104 +1,108 @@
+<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="10000" id="bs-carousel">
+  <!-- Overlay -->
+  <!-- <div class="overlay"></div> -->
 
-<!--   /HEADER   -->
-<div class="row">
-    <div class="carousel fade-carousel slide">
-        <!-- Overlay -->
-        <div class="overlay"></div>
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#bs-carousel" data-slide-to="1"></li>
+    <li data-target="#bs-carousel" data-slide-to="2"></li>
+    <li data-target="#bs-carousel" data-slide-to="3"></li>
+    <li data-target="#bs-carousel" data-slide-to="4"></li>
+</ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item slides active">
-                <div class="slide-1"></div>
-                <div class="hero">
-                    <hgroup>
-                        <h1>We are Groot</h1>        
-                        <h3>Get start your next awesome project</h3>
-                    </hgroup>
-                </div>
-            </div>
-        </div> 
-    </div>
+<!-- Wrapper for slides -->
+<div class="carousel-inner">
+    <div class="item slides active">
+      <div class="slide-1"></div>
+      <div class="hero">
+        <hgroup>
+          <h1>Introduction</h1>        
+          <h3>Intermark is a one of the kind mixed-use building that combines artistic facade and modern contemporary architecture concept that simply creates a beautiful modern time signature in the south-end of Jakarta Intermark is a one of the kind mixed-use bu</h3>
+          <a href="" data-toggle="modal" data-target="#myModal">Learn more</a>
+      </hgroup>
+  </div>
 </div>
-<!--   /call-out   -->
-<div class="container-fluid">
-    <div class="conts">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <?php foreach ($posts as $row): ?>
-                    <?php if ($row['posts_image'] != NULL) { ?>
-                        <div class="col-md-12">
-                            <div class="post">
-                                <div class="col-md-3">
-                                    <div class="imgLiquidFill imgLiquid" style="width:250px; height:185px;">
-                                        <img src="<?php echo $row['posts_image'] ?>" class="img-responsive">
-                                    </div>
-                                </div>
-                                <div class="col-md-9">
-                                    <div class="tag">
-                                        <h2><a href="<?php echo posts_url($row) ?>"><?php echo $row['posts_title'] ?></a></h2>
-                                        <div class="row-fluid">
-                                            <h5>
-                                                <span class=""><?php echo pretty_date($row['posts_published_date'], 'l, d-M-Y', FALSE) ?> </span> - 
-                                                <span class="">Category: <a href="<?php echo site_url('posts/category/' . $row['category_id']) ?>"><?php echo $row['category_name'] ?></a> </span>
-                                            </h5>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="up-teks">
-                                                    <p align="justify"><?php echo strip_tags(character_limiter($row['posts_description'], 500)) ?></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php } else { ?>
-                        <div class="col-md-12">
-                            <div class="post">
-                        <div class="col-md-12">
-                                <div class="tag">
-                                    <h2><a href="<?php echo posts_url($row) ?>"><?php echo $row['posts_title'] ?></a></h2>
-                                    <div class="row-fluid">
-                                        <h5>
-                                            <span class=""><?php echo pretty_date($row['posts_published_date'], 'l, d-M-Y', FALSE) ?> </span> - 
-                                            <span class="">Category: <a href="<?php echo site_url('posts/category/' . $row['category_id']) ?>"><?php echo $row['category_name'] ?></a> </span>
-                                        </h5>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="up-teks">
-                                                <p align="justify"><?php echo strip_tags(character_limiter($row['posts_description'], 600)) ?></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php } ?>
-                <?php endforeach; ?>
-            </div>
+<div class="item slides">
+  <div class="slide-2"></div>
+  <div class="hero">        
+    <hgroup>
+      <h1>Merdeka Assembly Hall</h1>        
+      <h3>Merdeka Assembly Hall is a grand multi function ballroom in the middle of Intermark with commercially used pre-function area in the front and function deck at the rear side that able to host more than 1,000 people.</h3>
+      <a href="" data-toggle="modal" data-target="#myModal">Learn more</a>
+  </hgroup>       
+</div>
+</div>
+<div class="item slides">
+  <div class="slide-3"></div>
+  <div class="hero">        
+    <hgroup>
+      <h1>Swiss-BelHotel</h1>        
+      <h3>Swiss-Belhotel is a superior 14 storey four-star hotel services and amenities in Intermark with modern contemporary architecture and three level basement designs.</h3>
+      <a href="" data-toggle="modal" data-target="#myModal">Learn more</a>
+  </hgroup>
+</div>
+</div>
+<div class="item slides">
+  <div class="slide-4"></div>
+  <div class="hero">        
+    <hgroup>
+      <h1>Shop-Houses</h1>        
+      <h3>24/7 Alfresco Dining is a deluxe three storey high shop house with 1 semi basement in a modern contemporary stacking boxes architecture in the front side of Intermark.</h3>
+      <a href="" data-toggle="modal" data-target="#myModal">Learn more</a>
+  </hgroup>
+</div>
+</div>
+<div class="item slides">
+  <div class="slide-5"></div>
+  <div class="hero">        
+    <hgroup>
+      <h1>Apartemen</h1>        
+      <h3>Tuscany Residence is a luxurious 21 storey high apartment in Intermark that offers ad-hock four-star hotel services and amenities in a modern contemporary architecture and three level basement design.</h3>
+      <a href="" data-toggle="modal" data-target="#myModal">Learn more</a>
+  </hgroup>
+</div>
+</div>
 
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="foots">
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="foots-icon pull-right">
-                            <span><a href=""><i class="ion-social-github"></i></a></span>
-                            <span><a href=""><i class="ion-social-twitter"></i></a></span>
-                            <span><a href=""><i class="ion-social-facebook"></i></a></span>
-                            <span><a href=""><i class="ion-social-googleplus"></i></a></span>
-                            <span><a href=""><i class="ion-social-instagram"></i></a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+</div> 
+</div>
 
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
     </div>
+    <div class="modal-body">
+     <div style="position:relative;height:0;padding-bottom:58.29%"><iframe src="https://www.youtube.com/embed/Q6ZnKIbvXrc?ecver=2" style="position:absolute;width:100%;height:100%;left:0" width="618" height="360" frameborder="0" allowfullscreen></iframe></div>
+ </div>
+ <div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"  id="onload">
+
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+     <!--    <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">×</button>
+          <h4 class="modal-title"><i class="fa fa-exclamation-circle"></i>Your title goes here</h4>
+      </div> -->
+      <div class="modal-body">
+        <img src="<?php echo base_url() ?>media/templates/intermark/images/iklan.jpg" class="img-responsive" width="100%">
+     </div>
+ <!--     <div class="modal-footer">
+      <button type="button" class="btn btn-default" data-dismiss="modal">Submit</button>
+  </div> -->
+</div>
+
 </div>
 </div>
