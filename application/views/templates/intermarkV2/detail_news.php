5eab6a7181efd794a79854bfbb6bbae2efa1e56c
Bopas <nazihbopas@gmail.com>
2017-09-08 11:21:58 +0700

    new
    new

---
+++ application/views/templates/intermarkV2/detail_news.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title> Intermark Indonesia</title>
+  <link rel="icon" href="media/images/ico/Nazih.png" type="icon">
+  <!-- Bootstrap Core CSS -->
+  <link href="media/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="media/css/ionicons.min.css" rel="stylesheet" type="text/css">
+  <link href="media/css/font-awesome.css" rel="stylesheet" type="text/css">
+  <link href="media/css/animate.min.css" rel="stylesheet" type="text/css">
+  <!-- <link href="media/css/material.indigo-pink.min.css" rel="stylesheet" type="text/css"> -->
+
+  <!-- Custom CSS -->
+  <link href="media/css/custom.css" rel="stylesheet" type="text/css">
+
+  <!-- Core JavaScript Files -->
+  <script type="text/javascript" src="media/js/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="media/js/bootstrap.min.js"></script>
+
+  <!-- Custom Theme JavaScript -->
+  <!-- <script type="text/javascript" src="media/js/material.min.js"></script> -->
+  <script type="text/javascript" src="media/js/wow.js"></script>
+  <script type="text/javascript" src="media/js/jquery.easing.min.js"></script>
+  <script type="text/javascript" src="media/js/custom.js"></script>
+
+
+
+</head>
+<body>
+
+
+  <!--bg img  -->
+  <div class="container-fluid main">
+    <nav class="top-bar">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-xs-5 hidden-xs">
+            <ul class="tools">     
+              <li>
+                <a class="" href="#"> <small> +123 4567 89108088</small> </a>
+              </li>
+            <!-- <li>
+               <a class="" href="#"><i class="fa fa-envelope" aria-hidden="true"></i><small> intermark@gmail.com</small></a>
+             </li> -->
+           </ul>
+         </div>
+         <div class="col-sm-4 text-center">
+         </div>
+         <div class="col-sm-4 col-xs-12 text-right">
+          <ul class="tools">                    
+           <li>
+             <a href="default.php"><small>Home</small></a>                  
+           </li>
+           <li>
+             <a href="career.php"><small>Career</small></a>                  
+           </li>
+           <li>
+             <a href="news.php"><small>News</small></a>                  
+           </li>
+           <li>
+             <a href="contact.php"><small>Contact</small></a>                  
+           </li>                   
+         </ul>
+       </div>
+     </div>
+   </div>
+ </nav>   <!--TOP-NAVBAR-END-->
+</div>
+
+<!--====================== NAVBAR MENU START===================-->
+<nav class="navbar navbar-default">
+  <div class="container">
+   <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>                        
+    </button>
+    <a class="navbar-brand" href="#"><img src="media/images/ico/Nazih.png" class=" hidden-xs" width="10%" style="margin-top: -16px;"></a>
+  </div>
+  <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown">Associate Tower<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="ion ion-location"></i> Lokasi</a></li>
+          <li><a href="#"><i class="ion ion-cash"></i> Harga</a></li>
+          <li><a href="#"><i class="ion ion-image"></i> Galeri</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown">Tuscany Residence<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="ion ion-location"></i> Lokasi</a></li>
+          <li><a href="#"><i class="ion ion-cash"></i> Harga</a></li>
+          <li><a href="#"><i class="ion ion-image"></i> Galeri</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown">Swiss Bell Hotell<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="ion ion-location"></i> Lokasi</a></li>
+          <li><a href="#"><i class="ion ion-cash"></i> Harga</a></li>
+          <li><a href="#"><i class="ion ion-image"></i> Galeri</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown">Merdeka Assembly Hall<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="ion ion-location"></i> Lokasi</a></li>
+          <li><a href="#"><i class="ion ion-cash"></i> Harga</a></li>
+          <li><a href="#"><i class="ion ion-image"></i> Galeri</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown">Shop Hause & Commercial<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="ion ion-location"></i> Lokasi</a></li>
+          <li><a href="#"><i class="ion ion-cash"></i> Harga</a></li>
+          <li><a href="#"><i class="ion ion-image"></i> Galeri</a></li>
+        </ul>
+      </li>     
+    </ul>
+  </div>
+</div>
+</nav>
+
+
+
+<section class="about-section" style="margin-top: 20px">
+  <div class="container">
+    <div class="col-lg-12">
+      <center>
+       <div class="index-content">
+        <div class="card">
+          <img src="media/images/0.jpg" class="img-responsive" width="100%" style="height: 500px; min-height: 150px; overflow: hidden;">
+          <h4>Investment Strategy</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <br>
+        </div>
+      </div>
+    </center>
+
+
+    <div class="row">
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <a href="http://www.youtube.com/watch?v=TdY3B2pg7Ko">
+          <img class="img-responsive pass" src="media/images/0.jpg" />
+        </a>
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/1.jpg" />
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/2.jpg" />
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/3.jpg" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/4.jpg" />
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/5.jpg" />
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/footer2.png" />
+      </div>
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive pass" src="media/images/train.png" />
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="container-fluid text-center bg-lightgray">
+  <div class="copyrights" style="margin-top:25px;">
+    <p>Intermark Indonesia © 2017, All Rights Reserved
+      <br>
+      <span>Themes : RumahCG</span></p>
+    </div>
+  </footer>
+
+</body>
+</html>
