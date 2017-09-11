2c23c22dad1ad2237820cc5a392d144613253e13
Bopas <nazihbopas@gmail.com>
2017-09-11 13:21:12 +0700

    update footer
    update footer support

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -119,7 +119,7 @@
                 <!-- Footer -->
                 <footer class="bottom" style="background-color: #2c3e50;">
                     <div class="orc text-center">
-                        <p class="text-muted" style="color: white;">© 2014-2015 GROOT - Supported by Artikulpi</a></p>
+                        <p class="text-muted" style="color: white;">© 2016-2017 GROOT - Supported by Rumah Cipta Generasi</a></p>
                     </div>
                 </footer>
             </body>
