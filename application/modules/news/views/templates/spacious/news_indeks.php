8d16d23c0e5e568832a62d566dd1fd1b491d32f0
Rahman Ade Saputra <utarahman@gmail.com>
2017-09-11 12:23:04 +0700

    ngilangin error 1

--- application/modules/news/views/templates/spacious/news_indeks.php
+++ application/modules/news/views/templates/spacious/news_indeks.php
@@ -13,16 +13,15 @@
 
                     <li>
                         <time datetime="2014-07-20">
-                            <span class="days"><?php echo pretty_date($row['news_published_date'], 'd', FALSE) ?></span>
-                            <span class="months"><?php echo pretty_date($row['news_published_date'], 'M', FALSE) ?></span>
+                            <span class="days"><?php echo pretty_date($row['news_post_date'], 'd', FALSE) ?></span>
+                            <span class="months"><?php echo pretty_date($row['news_post_date'], 'M', FALSE) ?></span>
                         </time>
                         <?php if ($row['news_image'] != NULL) { ?>
                             <img src="<?php echo $row['news_image'] ?>" />
                         <?php } ?>
                         <div class="info">
                             <h3 class="title"><a href="<?php echo news_url($row) ?>" > <?php echo character_limiter($row['news_title'], 40) ?></a></h3>
-                            <p><span class="ion-pricetags"></span> Kategori: <a href="<?php echo site_url('news/category/' . $row['category_id']); ?>"><?php echo $row['category_name'] ?></a></p>
-                            <p class="desc"><?php echo strip_tags(character_limiter($row['news_short_desc'], 130)) ?></p>
+                            <p class="desc"><?php echo strip_tags(character_limiter($row['news_description'], 130)) ?></p>
                         </div>
                     </li>
                 <?php endforeach ?>
