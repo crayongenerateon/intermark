736efc7c52698a39c1a1f6cb33f64022ee047806
Bopas <nazihbopas@gmail.com>
2017-10-03 20:37:32 +0700

    update form
    update form

--- application/modules/contact/views/templates/spacious/contact_detail.php
+++ application/modules/contact/views/templates/spacious/contact_detail.php
@@ -10,16 +10,16 @@
     <div class="col-md-7">
       <div class="row">
         <?php if ($this->session->flashdata('incomplete')) { ?>
-            <div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <?php echo $this->session->flashdata('incomplete') ?>
-            </div>
+        <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <?php echo $this->session->flashdata('incomplete') ?>
+        </div>
         <?php } elseif ($this->session->flashdata('complete')) { ?>
-            <div class="alert alert-success alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                Data diri anda telah kami simpan, silahkan tunggu balasan kami.
-            </div>
-            <?php }
+        <div class="alert alert-success alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          Data diri anda telah kami simpan, silahkan tunggu balasan kami.
+        </div>
+        <?php }
         ?>
         <?php
         $div_input = array('class' => 'form-group col-md-6 col-sm-12 col-xs-12');
@@ -28,10 +28,10 @@
         $name_data = array('name' => 'name', 'class' => 'form-control', 'placeholder' => 'Full Name');
         $email_data = array('name' => 'email', 'class' => 'form-control', 'placeholder' => 'Email');
         $textarea_data = array('name' => 'message', 'class' => 'form-control', 'rows' => 15, 'placeholder' => 'Massage');
-        $submit_data = array('class' => 'form-control btn btn-default', 'value' => 'Send Massage');
+        $submit_data = array('class' => 'form-control btn btn-success', 'value' => 'Send Massage');
         echo form_contact(current_url(), '', $name_data, $email_data, $textarea_data, $submit_data, $div_input, $div_submit, $div_textarea)
         ?>
-    </div>
+      </div>
     </div>
     <div class="col-md-5">
      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6501855780707!2d106.6873393142591!3d-6.309604063502476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69e531ee63e259%3A0x10ca1514be4fe7a3!2sIntermark+Indonesia!5e0!3m2!1sid!2sid!4v1505190136645" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
