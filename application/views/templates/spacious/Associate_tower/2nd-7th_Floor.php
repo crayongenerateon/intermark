676be4ae77897d4965f3ab5ba858e2a17af97ef5
Bopas <nazihbopas@gmail.com>
2017-10-04 21:28:20 +0700

    update
    update

--- application/views/templates/spacious/Associate_tower/2nd-7th_Floor.php
+++ application/views/templates/spacious/Associate_tower/2nd-7th_Floor.php
@@ -5,7 +5,7 @@
         <div id="scroll-able" data-spy="scroll" data-target="#target_nav">
           <div class="col-lg-6 col-md-6 col-sm-6 zoom col-centered">
             <center>
-             <img src="<?php echo base_url() ?>\media\image\AssociateTower\2nd-7th Floor\AT_2-7Floor.JPG" class="img-responsive img-thumbnail animated bounceIn" alt="intermark" width="100%"><br>
+             <img src="<?php echo base_url() ?>\media\image\AssociateTower\2nd-7th Floor\AT_2-7Floor.JPG" class="img-responsive img-thumbnail animated bounceIn" alt="intermark" width="100%">
            </center>
          </div>
          <div class="col-lg-6 col-md-6 col-sm-6 zoom col-centered">
