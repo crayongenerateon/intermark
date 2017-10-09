12c9c782413328aa00907b3702925c5347940b77
Bopas <nazihbopas@gmail.com>
2017-10-09 22:50:32 +0700

    update
    update

--- application/modules/news/views/templates/spacious/news_detail.php
+++ application/modules/news/views/templates/spacious/news_detail.php
@@ -1,33 +1,46 @@
 <section class="about-section" style="margin-top: 120px">
-    <div class="container">
-      <div class="col-lg-12">
-        <center>
-         <div class="index-content">
-          <div class="card" style="text-align: left;">
-            <img src="<?php echo $news['news_image'] ?>" class="img-responsive" width="100%" style="width: auto; height: auto; overflow: hidden;">
-            <h4><?php echo $news['news_title'] ?> <small class="text-danger pull-right"><i><?php echo pretty_date($news['date_created'], 'l, d-m-Y', FALSE) ?></i></small></h4>
-            <p><?php echo $news['news_content'] ?></p>
-            <br>
-          </div>
+  <div class="container">
+    <div class="col-lg-12">
+      <center>
+       <div class="index-content">
+        <div class="card" style="text-align: left;">
+          <img src="<?php echo $news['news_image'] ?>" class="img-responsive" width="100%" style="width: auto; height: auto; overflow: hidden;">
+          <h4><?php echo $news['news_title'] ?> <small class="text-danger pull-right"><i><?php echo pretty_date($news['date_created'], 'l, d-m-Y', FALSE) ?></i></small></h4>
+          <p><?php echo $news['news_content'] ?></p>
+          <br>
         </div>
-      </center>
+      </div>
+    </center>
 
-      <div class="row">
+    <div class="row">
       <?php $no = 0 ?>
       <?php foreach ($image as $row):?>
         <div class="col-md-3 col-sm-4 col-xs-6">
           <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="This is my title" data-caption="Some lovely red flowers" data-image="<?php echo upload_url($row['news_image_path']) ?>" data-target="#image-gallery">
-          <img class="img-responsive pass" src="<?php echo upload_url($row['news_image_path']) ?>" />
-        </a>
+            <img class="img-responsive pass" src="<?php echo upload_url($row['news_image_path']) ?>" />
+          </a>
         </div>
-      <?php $no++; ?>
+
+        
+        <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <center>
+                  <img id="image-gallery-image" class="img-responsive" src="<?php echo upload_url($row['news_image_path']) ?>">
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php $no++; ?>
       <?php endforeach ?>
-      </div>
-
-
-
     </div>
+
+
+
   </div>
+</div>
 </section>
 
 
@@ -59,30 +72,3 @@
 
 
 
-<div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="image-gallery-title"></h4>
-            </div>
-            <div class="modal-body">
-                <img id="image-gallery-image" class="img-responsive" src="<?php echo upload_url($row['news_image_path']) ?>">
-            </div>
-            <!-- <div class="modal-footer">
-
-                <div class="col-md-2">
-                    <button type="button" class="btn btn-primary" id="show-previous-image">Previous</button>
-                </div>
-
-                <div class="col-md-8 text-justify" id="image-gallery-caption">
-                    This text will be overwritten by jQuery
-                </div>
-
-                <div class="col-md-2">
-                    <button type="button" id="show-next-image" class="btn btn-default">Next</button>
-                </div>
-            </div> -->
-        </div>
-    </div>
-</div>
